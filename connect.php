a7eb3e29739863a0d186a1e61828f2677b5b4a83
Utkarsh Shresth <shresthutk62@gmail.com>
2022-01-20 22:47:20 +0530

    changed connent.php file after hosting the site

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$user='root';
-$host='localhost';
-$password='';
-$database='phpdata';
+$user='epiz_30863369';
+$host='sql309.epizy.com';
+$password='W2mxBnHZj6ADZ';
+$database='epiz_30863369_phpdata';
 $con=mysqli_connect($host,$user,$password,$database);
 if(!$con){
     echo 'Failed to connect';
